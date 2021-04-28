0029b77d44a615a6353712fb774dfa6c8b61b7c0
Dianne <dianne.diazm@gmail.com>
2021-04-28 15:55:56 -0400

    data facherita

--- src/components/Data.vue
+++ src/components/Data.vue
@@ -1,26 +1,34 @@
 <template>
   <v-container>
     <h1>Data</h1>
-    <div
-    v-for="dato in allDatos"
-    v-bind:key="dato.key"
-    >
-    {{ dato.title }}
-    </div>
+
+    <v-row no-gutters>
+      <v-col v-for="n in 3" :key="n" cols="12" sm="6" md="4">
+        <v-card
+          class="pa-2 ma-3"
+          outlined
+          tile
+          v-for="dato in allDatos"
+          v-bind:key="dato.key"
+        >
+          {{ dato.title }}
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
-import { mapGetters, mapActions } from 'vuex';
+import { mapGetters, mapActions } from "vuex";
 export default {
   name: "Data",
   methods: {
-    ...mapActions(["fetchDatos"])
+    ...mapActions(["fetchDatos"]),
   },
   computed: mapGetters(["allDatos"]),
   created() {
     this.fetchDatos();
-  }
+  },
 };
 </script>
 
